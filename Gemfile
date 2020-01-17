5bfefe1bd90d87dbbe725bd16a749d0bbd1aee80
EC2 Default User <ec2-user@ip-172-31-33-213.us-east-2.compute.internal>
2020-01-17 04:11:23 +0000

    メッセージコントローラ修正

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ group :development, :test do
   gem 'pry-doc'
   gem 'pry-byebug'
   gem 'pry-stack_explorer'
+  gem 'brakeman',  :require => false
 end
 
 group :development do
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,15 @@
 class MessagesController < ApplicationController
   def create
-    if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
-      @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
-      redirect_to "/rooms/#{@message.room_id}"
+    if Entry.where(:user_id => current_user.id, :room_id => message_params[:room_id]).present?
+      @message = Message.create(message_params)
     else
-      redirect_back(fallback_location: root_path)
+      flash[:alert] = "メッセージの送信に失敗しました"
     end
-  end    
+    redirect_to "/rooms/#{@message.room_id}"
+  end
+  
+  private
+  def message_params
+    params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id)
+  end
 end
