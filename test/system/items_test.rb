ec056548b1457ed30ac72f972083a47de2918547
EC2 Default User <ec2-user@ip-172-31-33-213.us-east-2.compute.internal>
2020-01-31 06:28:05 +0000

    itemテストのエラーをクリア

--- test/system/items_test.rb
+++ test/system/items_test.rb
@@ -13,24 +13,23 @@ class ItemsTest < ApplicationSystemTestCase
    login_user(@user)
    click_on '投稿する'
    assert_selector "h1", text: "投稿作成"
-   find("#item_image").click
-   attach_file "item[image]", "roe-19734_640.jpg"
+   find("#item_image")
+   attach_file "item[image]", "test/files/test.jpg"
    fill_in 'item[product]', with: item[:product]
    fill_in 'item[price]', with: item[:price]
    fill_in 'item[description]', with: item[:description]
    click_button '投稿'
-   page.save_screenshot "tmp/screenshots/#{Time.now.strftime('%Y%m%d%H%M%S')}.png"
  end
  
  test "visiting the new" do
    visit new_item_path
    assert_selector "h1", text: "投稿作成"
  end
- 
+
  test "create item" do
    item_test(@item)
    assert_text "商品を投稿しました"
  end
  
- 
+
 end
