7bac9c79c2f7a690ac4f12fa5a07c8e4f004ce65
EC2 Default User <ec2-user@ip-172-31-33-213.us-east-2.compute.internal>
2020-02-07 13:37:07 +0000

    testsessionsコントローラーに記述

--- app/controllers/testsessions_controller.rb
+++ app/controllers/testsessions_controller.rb
@@ -1,2 +1,8 @@
 class TestsessionsController < ApplicationController
+  def create
+    user=User.find_by(email:"test@example.com")
+    session[:user_id] = user.id
+    flash[:success] = "テストユーザとしてログインしました。"
+    redirect_to user
+  end
 end
