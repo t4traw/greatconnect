1045eb5879b60f4cad1e3911b0e75321b6fa5d45
EC2 Default User <ec2-user@ip-172-31-33-213.us-east-2.compute.internal>
2020-02-07 11:18:30 +0000

    items_test.rbにテスト追加

--- test/system/messages_test.rb
+++ test/system/messages_test.rb
@@ -2,52 +2,6 @@ require "application_system_test_case"
 
 class MessagesTest < ApplicationSystemTestCase
   
-  setup do
-    @user2 = {
-      name: "テストユーザ2",
-      age: 21,
-      #from: "大阪府",
-      email: "test21@gmail.com",
-      password: "testtest21"
-    }
-    
-    @message = {
-      content: "テストです"
-    }
-    
-  end
-  
-  def register_user2(user2)
-    visit root_url
-    click_on '登録する'
-    assert_selector "h1", text: "新規登録"
-    fill_in 'user[name]', with: user2[:name]
-    fill_in 'user[age]', with: user2[:age]
-    select '大阪府', from: '都道府県'
-    fill_in 'user[email]', with: user2[:email]
-    fill_in 'user[password]', with: user2[:password]
-    fill_in 'user[password_confirmation]', with: user2[:password]
-    click_button '登録'
-  end
-  
-  def login_user2(user2)
-    register_user2(@user2)
-    visit root_url
-    click_on 'ログインする'
-    assert_selector "h1", text: "ログイン"
-    fill_in 'session[email]', with: user2[:email]
-    fill_in 'session[password]', with: user2[:password]
-    click_button 'ログイン'
-  end
-  
-  def message_user(user2)
-    item_test(@item)
-    click_on 'ログアウト'
-    login_user2(@user2)
-    visit items_url
-    click_on "テストユーザー"  
-  end
-  
   test "visiting the show" do
     message_user(@user2)
     assert_selector "h2", text: "テストユーザーさんのページ"
