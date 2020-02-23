02378f444ea73c524f076f69500b98f4c6bdb00f
EC2 Default User <ec2-user@ip-172-31-33-213.us-east-2.compute.internal>
2020-02-23 09:48:45 +0000

    S3設定の見直し

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,15 @@
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1'
-    }
-    
-    config.fog_directory = 'greatconnect'
-    config.cache_storage = :fog
-  end
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: 'ap-northeast-1',
+    path_style: true
+  }
+  
+  config.fog_directory = 'greatconnect'
+  config.storage :fog
+  config.fog_provider = 'fog/aws'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/greatconnect'
+  
 end
